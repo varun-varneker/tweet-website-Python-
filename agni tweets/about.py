b4434abe1427776b8db550285ff69f1fed9d8906
varun <varunvarneker06@gmail.com>
2024-09-02 22:45:46 +0530

    Add files via upload

---
+++ agni tweets/about.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.subheader('Agni college of technology :violet[Thoughts] ')
+    st.subheader('here you can share your thoughts on collge')
+    st.markdown('Home Link: [Click Here](https://act.edu.in/)')
+    st.markdown('Contact via mail: [mail@act.edu.in]')
+    
